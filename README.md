e1c740504b769cb840da41b2162174e64116af4b
Erik Hennerfors <erik.hennerfors@obscured.se>
2020-12-20 12:40:23 +0100

    First code commit

---
+++ README.md
@@ -0,0 +1,12 @@
+﻿# laget.Limiter.Store.Mongo
+MongoDB store implementation for laget.Limiter...
+
+
+## Usage
+> You can specifiy that a limiter should be used by the following
+```c#
+builder.Register<ISomeLimit>(c =>
+    new AuthorizationLimit(new MongoStore(new MongoUrl(c.Resolve<IConfiguration>().GetConnectionString("MongoConnectionString")), "authorization.calls"),
+        new StandardLimit(300, TimeSpan.FromHours(3)))
+).SingleInstance();
+```
---
+++ laget.Limiter.Stores/MongoStore.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace laget.Limiter.Stores
+{
+    public class Call
+    {
+        [BsonElement("id"), BsonId, BsonIgnoreIfDefault, BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("createdAt"), BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [BsonElement("value")]
+        public long Value { get; set; }
+    }
+
+    public class MongoStore : IStore
+    {
+        readonly IMongoCollection<Call> _store;
+
+        public MongoStore(MongoUrl url, string collection = "calls")
+        {
+            var client = new MongoClient(url);
+
+            var database = client.GetDatabase(url.DatabaseName, new MongoDatabaseSettings
+            {
+                ReadConcern = ReadConcern.Default,
+                ReadPreference = ReadPreference.SecondaryPreferred,
+                WriteConcern = WriteConcern.Acknowledged
+            });
+
+            _store = database.GetCollection<Call>(collection);
+
+            EnsureIndexes();
+        }
+
+        public void Add(DateTime item)
+        {
+            _store.InsertOne(new Call { Value = item.ToBinary() });
+        }
+
+        public IList<DateTime> Get() => _store.Find(new BsonDocument()).ToList()
+                .Select(x => DateTime.FromBinary(x.Value))
+                .ToList();
+
+        public void Trim(int amount)
+        {
+            var items = Get()
+                .OrderByDescending(x => x)
+                .Take(amount)
+                .Select(x => x.ToBinary())
+                .ToList();
+
+            var builder = Builders<Call>.Filter;
+            var filter = builder.Nin(x => x.Value, items);
+
+            _store.DeleteMany(filter);
+        }
+
+        void EnsureIndexes()
+        {
+            var builder = Builders<Call>.IndexKeys;
+            var indexes = new List<CreateIndexModel<Call>>
+            {
+                new CreateIndexModel<Call>(builder.Ascending(_ => _.CreatedAt), new CreateIndexOptions { Background = true, ExpireAfter = TimeSpan.FromHours(3) }),
+                new CreateIndexModel<Call>(builder.Ascending(_ => _.Value), new CreateIndexOptions { Background = true, Unique = true })
+            };
+            _store.Indexes.CreateMany(indexes);
+        }
+    }
+}
